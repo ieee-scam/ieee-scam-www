a63271db2ad01cd842edfc49daefd08a1ee5a465
Davy Landman <davy.landman@gmail.com>
2012-11-22 09:16:16 +0100

    Improved mobile look of pages by not loading the facebook iframe

---
+++ 2013/script/main.js
@@ -0,0 +1,24 @@
+var isMedia = (function(){
+	var div;
+
+	return function(query){
+
+		//if the <div> doesn't exist, create it and make sure it's hidden
+		if (!div){
+			div = document.createElement("div");
+			div.id = "ncz1";
+			div.style.cssText = "position:absolute;top:-1000px";
+			document.body.insertBefore(div, document.body.firstChild);            
+		}
+
+		div.innerHTML = "_<style media=\"" + query + "\"> #ncz1 { width: 1px; }</style>";
+		div.removeChild(div.firstChild);
+		return div.offsetWidth == 1;    
+	};
+})();
+if (isMedia("only screen and (max-width:640px)") || isMedia("screen and (max-device-width:640px)")) {
+	var facebook = document.getElementById("facebook");
+	if (facebook) {
+		facebook.parentNode.removeChild(facebook);	// avoid iFrame loading on mobile pages
+	}
+}
