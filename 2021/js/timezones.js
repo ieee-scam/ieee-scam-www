b2aa9b02dd44ca78e27af516335c65a4178fd40c
Nathan Cassee <n.w.cassee@tue.nl>
2021-09-16 14:44:45 +0200

    more bugfixing

--- 2021/js/timezones.js
+++ 2021/js/timezones.js
@@ -28,7 +28,8 @@ function setTimezoneInItem(item, utcOffset) {
     start_time = addMinutes(start_time, offsetInMinutes);
     end_time = addMinutes(end_time, offsetInMinutes);
 
-    item.innerHTML = start_time.getHours() + ":" + start_time.getMinutes() < 10 ? "0" + start_time.getMinutes() : start_time.getMinutes() + " - " + end_time.getHours() + ":" + end_time.getMinutes() ? "0" + end_time.getMinutes() : end_time.getMinutes();
+    item.innerHTML = start_time.getHours() + ":" + (start_time.getMinutes() < 10 ? "0" + start_time.getMinutes() : start_time.getMinutes()) + " - " + 
+                end_time.getHours() + ":" + (end_time.getMinutes() ? "0" + end_time.getMinutes() : end_time.getMinutes());
 }
 
 convertTableItems(2);
