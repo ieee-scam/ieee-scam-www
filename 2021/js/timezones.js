a8f4bf8ebf182f7879af9d94b481763a91a1fbcf
Nathan Cassee <n.w.cassee@tue.nl>
2021-09-16 14:30:10 +0200

    Add timezone script

---
+++ 2021/js/timezones.js
@@ -0,0 +1,34 @@
+
+function convertTableItems(utcOffset) {
+    items = document.querySelectorAll("#program-table .duration");
+
+    items.forEach(function (item) {
+        setTimezoneInItem(item, utcOffset);
+    });
+}
+
+function addMinutes(date, minutes) {
+    return new Date(date.getTime() + minutes*60000);
+}
+
+function setTimezoneInItem(item, utcOffset) {
+    item = item.querySelectorAll("p")[0];
+
+    var time = item.getAttribute("data-start");
+    var durationInMinutes = item.getAttribute("data-duration");
+
+    var hour = time.split(":")[0];
+    var minutes = time.split(":")[1];
+
+    start_time = new Date(Date.UTC('2020','01','01', hour, minutes,'30'));
+    end_time = addMinutes(start_time, parseInt(durationInMinutes));
+
+    offsetInMinutes = utcOffset * 60;
+
+    start_time = addMinutes(start_time, offsetInMinutes);
+    end_time = addMinutes(end_time, offsetInMinutes);
+
+    item.innerHTML = start_time.getHours() + ":" + start_time.getMinutes() + " - " + end_time.getHours + ":" + end_time.getMinutes();
+}
+
+convertTableItems(2);
